a65634e607bb3ba5ec92b7e9afa4c22aef29b86b
jxxyx <jxxyx21@gmail.com>
2024-03-22 00:42:49 +0800

    fix : updated UserRegistrationProcess.php

---
+++ UserRegistrationProcess.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+
+<head>
+  <meta charset="UTF-8">
+  <title> User Login and Sign Up</title>
+  <link rel="stylesheet" href="CSS/Login.css">
+  <?php
+  include "inc/header.inc.php";
+  ?>
+</head>
+
+<body>
+    <?php 
+    include "inc/nav.inc.php"; 
+    ?>
+    <main>
+        <?php 
+        
+        ?>
+    </main>
+</body>
