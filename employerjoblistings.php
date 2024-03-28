7cb238e3c432288a0fae00b56c60f63a57306efd
C-MingZhao <2303045@sit.singaporetech.edu.sg>
2024-03-28 19:41:34 +0800

    Update employerjoblistings.php
    Update Employer Job Listing to be in same format as the rest

--- employerjoblistings.php
+++ employerjoblistings.php
@@ -17,27 +17,11 @@
   <?php
   include "inc/nav.inc.php";
   ?>
-
-  <!-- Sidebar -->
-  <nav class="w3-sidebar w3-bar-block w3-collapse w3-large w3-theme-l5 w3-animate-left" id="mySidebar">
-    <a href="javascript:void(0)" onclick="w3_close()" class="w3-right w3-xlarge w3-padding-large w3-hover-black w3-hide-large" title="Close Menu">
-      <i class="fa fa-remove"></i>
-    </a>
-    <h4 class="w3-bar-item"><b>Status</b></h4>
-    <a class="w3-bar-item w3-button w3-hover-black" href="#">Jobs Listed</a>
-    <a class="w3-bar-item w3-button w3-hover-black" href="#">Shortlisted Candidates</a>
-    <a class="w3-bar-item w3-button w3-hover-black" href="#">Offered Jobs to Candidates</a>
-  </nav>
-
-  <!-- Overlay effect when opening sidebar on small screens -->
-  <div class="w3-overlay w3-hide-large" onclick="w3_close()" style="cursor:pointer" title="close side menu" id="myOverlay"></div>
-
-  <!-- Main content: shift it to the right by 250 pixels when the sidebar is visible -->
-  <div class="w3-main" style="margin-left:250px">
-
-
+<div class="w3-content" style="max-width:1400px;">
+  <div class="w3-row">
     <div class="w3-container w3-card w3-white w3-margin-bottom">
       <h2 class="w3-text-grey w3-padding-16"><i class="fa fa-paper-plane fa-fw w3-margin-right w3-xxlarge w3-text-teal"></i>Jobs Listed</h2>
+      <div class="w3-responsive">
       <table class="w3-table-all">
         <tr class="w3-theme">
           <th>Job Title</th>
